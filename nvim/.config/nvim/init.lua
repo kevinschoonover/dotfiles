8be020d28cc52459a933e92635f7b9321efdb0f4
Kevin Schoonover <me@kschoon.me>
2023-01-09 23:38:00 -0800

    load neovim plugins first since we assume that in config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,8 @@
 -- https://github.com/ThePrimeagen/init.lua
 -- https://github.com/nvim-lua/kickstart.nvim
---
+
+require("plugins")
+
 function is_directory(path)
   local stat = vim.loop.fs_stat(path)
   return stat and stat.type == "file" or false
@@ -107,6 +109,5 @@ vim.g.indent_blankline_filetype_exclude = { "help", "packer" }
 vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
 vim.g.indent_blankline_show_trailing_blankline_indent = false
 
-require("plugins")
 require("modules.init")
 require("filetype")
