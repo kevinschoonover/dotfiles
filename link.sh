8549b82862adce722d1b31b8654105e25b060a15
Kevin Schoonover <me@kschoon.me>
2023-01-15 21:24:12 -0800

    fix: use git + tmux configuration from dotfiles

--- link.sh
+++ link.sh
@@ -1,3 +1,6 @@
 for service in nvim sway waybar alacritty mako kitty; do
   ln -rs $service/.config/$service ~/.config/
 done
+
+ln -rs ./tmux/.tmux.conf ~/.tmux.conf
+ln -rs ./git/.gitconfig ~/.gitconfig
